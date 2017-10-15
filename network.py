48b8415b009cd3c6ea22d3aa9d5e0347587e8b9c
Cesar <cecerhcp0@hotmail.com>
2017-10-15 12:33:20 -0200

    metodo para escolha de voto a partir das conexoes ativas

--- network.py
+++ network.py
@@ -1,6 +1,6 @@
 import threading
 import socket
-
+import gvars as gl
 
 def getMyIP():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,3 +10,12 @@ def getMyIP():
 
 def getLanPrefix(myIP):
     return myIP[0:myIP.rfind('.')]
+
+
+def getMyVoteIP():
+	myIP = getMyIP()
+	lanPrefix = getLanPrefix(myIP)
+	connectedIPs = [int(ip.split(".")[-1]) for ip in gl.connected_ips if ip != gl.leader_ip]
+	voteSuffix = max(connectedIPs)
+	print(connectedIPs)
+	return (lanPrefix + "." + str(voteSuffix))
